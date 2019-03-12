256cfb6ac89fdfe4830c6e9c709f5be0de0490ae
PavelGnatyuk <pgnatyuk@gmail.com>
2019-03-12 07:53:56 +0200

    Initial commit

---
+++ WhoIsThis/WhoIsThis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhoIsThis
+//
+//  Created by Pavel Gnatyuk on 12/03/2019.
+//  Copyright © 2019 Pavel Gnatyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WhoIsThisKit/WhoIsThisKit/WhoIsThisKit.h
@@ -0,0 +1,19 @@
+//
+//  WhoIsThisKit.h
+//  WhoIsThisKit
+//
+//  Created by Pavel Gnatyuk on 12/03/2019.
+//  Copyright © 2019 Pavel Gnatyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WhoIsThisKit.
+FOUNDATION_EXPORT double WhoIsThisKitVersionNumber;
+
+//! Project version string for WhoIsThisKit.
+FOUNDATION_EXPORT const unsigned char WhoIsThisKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WhoIsThisKit/PublicHeader.h>
+
+
